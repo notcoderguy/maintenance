b05b6ff3f6d2b33484247197580376e554ef788c
NotCoderGuy <notcoderguy@gmail.com>
2023-05-22 18:46:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Maintenace
+
+This project is generic maintenance project for all the projects on the NCG Server.
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+      './src/**/*.{vue,js,ts,jsx,tsx,blade.php,html}',
+    ],
+    darkMode: 'class', // or 'media'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
